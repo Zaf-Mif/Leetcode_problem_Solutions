58fb902b2add15f3675995386d3e81a8d2836371
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-05-23 18:49:21 +0300

    Time: 2331 ms (5.02%) | Memory: 30.3 MB (6.78%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -1,13 +1,32 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        # we can make an operation k times
+        def check(i):
+            target = nums[i]
+            left, right, best = 0, i, i
+
+            while left <= right:
+                mid = (left + right) // 2
+                cnt = i - mid + 1
+                final = cnt * target
+                original = prefix[i] - prefix[mid] + nums[mid]
+                opr = final - original
+
+                if opr > k:
+                    left = mid + 1
+                else:
+                    best = mid 
+                    right = mid - 1
+
+            return i - best + 1
+        
         nums.sort()
-        left, curr = 0, 0
-        for right in range(len(nums)):
-            target = nums[right]
-            curr += target
-            if (right - left + 1) * target - curr > k:
-                curr -= nums[left]
-                left += 1
-        return len(nums) - left
+        prefix = [nums[0]]
+
+        for num in range(1,len(nums)):
+            prefix.append(prefix[-1] + nums[num]) 
+
+        ans = 0
+        for i in range(len(nums)):
+            ans = max(ans, check(i))
+        return ans
 
