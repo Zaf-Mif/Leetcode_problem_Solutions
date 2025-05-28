f473d09a426a2c24c35ae58b34c335b583138ebf
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-05-28 23:30:08 +0300

    Time: 1541 ms (79.20%) | Memory: 18.9 MB (42.40%) - LeetSync

---
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]], k: int) -> List[int]:
+
+        def dfs(node: int, parent: int, children: List[List[int]], k: int) -> int:
+            if k < 0:
+                return 0
+            res = 1
+            for child in children[node]:
+                if child == parent:
+                    continue
+                res += dfs(child, node, children, k - 1)
+            return res
+
+        def check(edges: List[List[int]], k: int) -> List[int]:
+            n = len(edges) + 1
+            children = [[] for _ in range(n)]
+            for u, v in edges:
+                children[u].append(v)
+                children[v].append(u)
+            res = [0] * n
+            for i in range(n):
+                res[i] = dfs(i, -1, children, k)
+            return res
+
+        n = len(edges1) + 1
+        cnt1 = check(edges1, k)
+        cnt2 = check(edges2, k - 1)
+        mxcnt2 = max(cnt2)
+        res = [cnt1[i] + mxcnt2 for i in range(n)]
+        return res
