06755305018cc3b3c10cd44f04f3aa0ea729cd5c
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-08-10 21:35:04 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (17.56%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        def signature(x):
+            return ''.join(sorted(str(x)))
+    
+        power_signatures = {signature(1 << i) for i in range(31)}
+        
+        return signature(n) in power_signatures
