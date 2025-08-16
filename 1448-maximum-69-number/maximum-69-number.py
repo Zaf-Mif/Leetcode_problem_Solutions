04428214d338e99a018a106adf89ae2891a4d913
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-08-16 10:40:32 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.80%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.py
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -1,18 +1,12 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        #  changing the first 6 to 9 make the num higher 
-        idx = 0
-        #  store the first occurence of 6 and change it to 9 
         nums = str(num)
-        for i in range(len(nums)):
-            if nums[i] == '6':
-                idx = i
-                break
-        #  iterate and change the idx to 9 
         ans = ''
+        first = False
         for i in range(len(nums)):
-            if i == idx and nums[idx] == "6": 
+            if first == False and nums[i] == "6": 
                 ans += '9'
+                first = True
                 continue
             ans += nums[i]
         # print(ans, idx , nums)
