bb8fe9b1802bc96c1326c6dfffdbaf4f7751c143
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-09-04 22:17:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (96.91%) - LeetSync

--- 3830-find-closest-person/find-closest-person.py
+++ 3830-find-closest-person/find-closest-person.py
@@ -3,9 +3,6 @@ class Solution:
         p1 = x-z if x > z else z-x
         p2 = y-z if y > z else z-y
 
-        if p1 < p2:
-            return 1
-        elif p1 > p2:
-            return 2
-        else:
-            return 0
+        if p1 < p2:  return 1
+        elif p1 > p2: return 2
+        else: return 0
