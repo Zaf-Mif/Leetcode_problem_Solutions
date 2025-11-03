958a7c2ef94b3d38163622933bc72f05011c0ebd
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-11-03 19:55:05 +0300

    Time: 108 ms (62.50%) | Memory: 26.4 MB (62.73%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,19 +1,12 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n = len(colors)
+        l = 0
         cnt = 0
-        grp, temp = [], colors[0]
-        for i in range(1, len(colors)):
-            if colors[i] == colors[i-1]:
-                temp += (colors[i])
-            else:
-                grp.append(temp)
-                temp = colors[i]
-        grp.append(temp)
-        # print(grp)
-        k = 0
-        for i in range(len(grp)):
-            # print(neededTime[k: k + len(grp[i])])
-            cnt += (max(neededTime[k: k + len(grp[i])]))
-            # print(cnt)
-            k += len(grp[i])
-        return sum(neededTime) - cnt
+        for r in range(n):
+            if colors[r] != colors[l]:
+                same = neededTime[l:r]
+                cnt += sum(same) - max(same)
+                l = r
+        cnt += sum(neededTime[l:]) - max(neededTime[l:])
+        return cnt 
