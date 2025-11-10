9cff156d2e2dee4da0b8a76fd4ff7cf0d7adc2d3
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-11-10 18:36:50 +0300

    Time: 3 ms (62.60%) | Memory: 17.9 MB (31.76%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.py
@@ -1,12 +1,10 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
+        res = [] 
         freq = defaultdict(int)
         for c in nums:
-            freq[c] += 1
-        
-        pairs = freq.items()
-        pairs = sorted(pairs, key=lambda item: (item[1], -item[0]))
-        res = []
+            freq[c] += 1 
+        pairs = sorted(freq.items(), key=lambda item: (item[1], -item[0]))
         for key, value in pairs:
             for i in range(value):
                 res.append(key)
