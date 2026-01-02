51355fed8c45f6deb6ddc52d57d2c23db67b2c98
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2026-01-02 18:26:08 +0300

    Time: 7 ms (47.84%) | Memory: 18.5 MB (84.85%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -1,11 +1,11 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
         # our n ln(nums)
-        n = len(nums) // 2
         #  uniques == n + 1 and 
         #  [1,2,3,3] n = 4 // 2 = 2 and 3 unique elements 
-        freq = defaultdict(int)
-        for c in nums:
-            freq[c] += 1
-        return next(key for key, value in freq.items() if value == n)
-        
+        cnt = collections.Counter(nums)
+        # print(cnt)
+        for c in cnt:
+            # print(c, cnt[c])
+            if cnt[c] > 1:
+                return c
